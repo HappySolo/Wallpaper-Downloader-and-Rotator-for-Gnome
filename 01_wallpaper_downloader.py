cae40c9c38f4250f2f104269c1b97de552f7dc1b
lukasz <lukasz@happylatte.com>
2013-01-10 14:55:15 +0800

    Add infite downloading to downloader

--- 01_wallpaper_downloader.py
+++ 01_wallpaper_downloader.py
@@ -3,7 +3,7 @@
 """
 wallpapers.py
 
-Download images from /r/EarthPorn (or from other reddits) and 
+Download images from /r/EarthPorn (or from other reddits) and
 create a rotating background XML for Gnome.
 
 See the README file for more details.
@@ -19,13 +19,14 @@ from wallpapers.scraper import dispatch
 from wallpapers.helper import directories
 from wallpapers.helper import resize
 
+from time import sleep
 
 cnt_resized_images = 0
 
 
 def is_ok_for_wallpaper(image_url):
     """Decide whether an image_url is appropriate as a wallpaper.
-    
+
     An image_url is good if (1) it's resolution is large enough,
     (2) rotation is landscape, and (3) ratio is OK.
     """
@@ -40,22 +41,22 @@ def is_ok_for_wallpaper(image_url):
         return False
     # else, if the image could be opened
     width, height = img.size
-    
+
     large = (width * height) >= minimum_pixels
     landscape = width > height
     ratio = float(width) / float(height)
     ratio_ok = (cfg.RATIO_INTERVAL[0] <= ratio <= cfg.RATIO_INTERVAL[1])
-    
+
     ok_for_wallpaper = (large and landscape and ratio_ok)
-        
+
     return ok_for_wallpaper
 
 
 def register_good_and_bad_image_urls_to_db(good_images, bad_images):
-    """Store image URLs in the DB and indicate if they are good or bad.""" 
+    """Store image URLs in the DB and indicate if they are good or bad."""
     for img in good_images:
         db.add_image(img)
-        
+
     for img in bad_images:
         db.add_image(img, good=False)
 
@@ -70,7 +71,7 @@ def delete_bad_images(bad_image_urls):
 
     print("# removed image(s): {0}".format(len(bad_image_urls)))
 
-    
+
 def download_images(image_urls):
     """Use wget to download new images into a specified directory."""
     fetched = []
@@ -103,13 +104,13 @@ def header():
     size = len(header) + 2 + 2
     return """{sep}
 # {header} #
-{sep}""".format(sep='#'*size, header=header) 
+{sep}""".format(sep='#'*size, header=header)
 
 
 def resize_large_images(image_urls):
     """Resize wallpaper images that are too large."""
     global cnt_resized_images
-    
+
     for image_url in image_urls:
         file_name = web.get_file_name(image_url)
         file_path = cfg.PHOTO_DIR + file_name
@@ -124,40 +125,45 @@ def get_images_from_site(site_key):
     """Get images from a given site."""
     cfg.set_current_choice(site_key)
     directories.check_photo_dir()
-    
+
     print header()
 
     all_image_urls = dispatch.get_images(cfg.get_current_site_record())
     # keep JPG files only:
     jpg_image_urls = [x for x in all_image_urls if x.lower().endswith('jpg')]
-    
+
     fetched_image_urls = download_images(jpg_image_urls)
 
     good_image_urls = [x for x in fetched_image_urls if is_ok_for_wallpaper(x)]
     bad_image_urls = list( set(fetched_image_urls).difference(set(good_image_urls)) )
-    
+
     register_good_and_bad_image_urls_to_db(good_image_urls, bad_image_urls)
     delete_bad_images(bad_image_urls)
-    
+
     if cfg.RESIZE_LARGE_IMAGES:
         resize_large_images(good_image_urls)
 
-    
+
 def main():
     """Control block."""
     cfg.self_verify()
     directories.check_base_dir()
     db.init()
-    
-    save_choice = cfg.CURRENT_CHOICE
-    
-    for site_key in cfg.MULTIPLE_CHOICE:
-        get_images_from_site(site_key)
-        
-    cfg.set_current_choice(save_choice)
-    
-    print "#"
-    print "# total number of resized images: {cnt}".format(cnt=cnt_resized_images)
+
+    while  True:
+        save_choice = cfg.CURRENT_CHOICE
+
+        for site_key in cfg.MULTIPLE_CHOICE:
+            get_images_from_site(site_key)
+
+        cfg.set_current_choice(save_choice)
+
+        print "#"
+        print "# total number of resized images: {cnt}".format(cnt=cnt_resized_images)
+        try:
+            sleep(float(60*60*6)) #every 6 hours
+        except KeyboardInterrupt:
+            sys.exit()
 
 #############################################################################
 
